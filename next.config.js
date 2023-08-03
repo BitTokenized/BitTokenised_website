4b41f4e35cd7a37d9c107b053302bf00e4df5b0d
caa1982 <cyrilalvarezadriaansen@gmail.com>
2023-08-03 12:40:46 +0100

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  assetPrefix: process.env.NODE_ENV === "production" ? "/my-next-app/" : "",
 };
 
 module.exports = nextConfig;
