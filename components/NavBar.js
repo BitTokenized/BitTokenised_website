ae7a75c1d890725902d273733aafe6a659705d3b
caa1982 <cyrilalvarezadriaansen@gmail.com>
2023-08-03 12:07:05 +0100

    postcss

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,42 +1,38 @@
-import React, { useState } from "react";
-import Link from "next/link";
+import React from "react";
 import { AiOutlineInfoCircle, AiOutlineHome } from "react-icons/ai";
+import { useRouter } from "next/router";
 
 const NavBar = () => {
-  const [isAboutPage, setIsAboutPage] = useState(false);
+  const router = useRouter();
+  const isAboutPage = router.pathname === "/about-us";
 
   const handleClick = () => {
-    setIsAboutPage(!isAboutPage);
+    if (isAboutPage) {
+      router.push("/");
+    } else {
+      router.push("/about-us");
+    }
   };
 
   return (
     <div className="navbar bg-transparent flex justify-between items-center">
-      <Link href="/">
+      <a href="/">
         <img
           src="images/logo_2.png"
           alt="Logo_2"
           className="ml-2 h-8 md:h-12"
         />
-      </Link>
+      </a>
       <button
         onClick={handleClick}
         className="flex items-center space-x-1 text-white hover:text-secondary transition-all duration-300 ease-in-out cursor-pointer"
       >
         {isAboutPage ? (
-          <>
-            <AiOutlineHome className="h-5 w-5" />
-            <Link href="/">
-              <span>Home</span>
-            </Link>
-          </>
+          <AiOutlineHome className="h-5 w-5" />
         ) : (
-          <>
-            <AiOutlineInfoCircle className="h-5 w-5" />
-            <Link href="/about-us">
-              <span>About Us</span>
-            </Link>
-          </>
+          <AiOutlineInfoCircle className="h-5 w-5" />
         )}
+        <span>{isAboutPage ? "Home" : "About Us"}</span>
       </button>
     </div>
   );
