7204bcf2f85b7de26d346e7566ff8cf200229a28
caa1982 <cyrilalvarezadriaansen@gmail.com>
2023-08-03 13:29:32 +0100

    using router

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,31 +1,39 @@
 import React from "react";
-import Link from "next/link";
 import { AiOutlineInfoCircle, AiOutlineHome } from "react-icons/ai";
+import { useRouter } from "next/router";
 
 const NavBar = () => {
-  const isAboutPage = window.location.pathname === "/about-us";
+  const router = useRouter();
+  const isAboutPage = router.pathname === "/about-us";
+
+  const handleClick = () => {
+    if (isAboutPage) {
+      router.push("/");
+    } else {
+      router.push("/about-us");
+    }
+  };
 
   return (
     <div className="navbar bg-transparent flex justify-between items-center">
-      <Link href="/">
-        <a>
-          <img
-            src="images/logo_2.png"
-            alt="Logo_2"
-            className="ml-2 h-8 md:h-12"
-          />
-        </a>
-      </Link>
-      <Link href={isAboutPage ? "/" : "/about-us"}>
-        <a className="flex items-center space-x-1 text-white hover:text-secondary transition-all duration-300 ease-in-out cursor-pointer">
-          {isAboutPage ? (
-            <AiOutlineHome className="h-5 w-5" />
-          ) : (
-            <AiOutlineInfoCircle className="h-5 w-5" />
-          )}
-          <span>{isAboutPage ? "Home" : "About Us"}</span>
-        </a>
-      </Link>
+      <a href="/">
+        <img
+          src="images/logo_2.png"
+          alt="Logo_2"
+          className="ml-2 h-8 md:h-12"
+        />
+      </a>
+      <button
+        onClick={handleClick}
+        className="flex items-center space-x-1 text-white hover:text-secondary transition-all duration-300 ease-in-out cursor-pointer"
+      >
+        {isAboutPage ? (
+          <AiOutlineHome className="h-5 w-5" />
+        ) : (
+          <AiOutlineInfoCircle className="h-5 w-5" />
+        )}
+        <span>{isAboutPage ? "Home" : "About Us"}</span>
+      </button>
     </div>
   );
 };
