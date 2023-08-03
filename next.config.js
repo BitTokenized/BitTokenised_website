9b4d86aa4095be37a5a92f8020154213e373e662
caa1982 <cyrilalvarezadriaansen@gmail.com>
2023-08-03 12:59:15 +0100

    remove gh_pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NODE_ENV === "production" ? "/my-next-app/" : "",
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
